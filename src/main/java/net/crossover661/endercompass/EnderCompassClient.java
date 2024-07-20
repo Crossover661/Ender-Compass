271fae3a38a2eb619b31218aa2280bf7a6a0dc59
Crossover661 <sanjay.k.srikanth@gmail.com>
2024-07-20 14:22:20 -0700

    Initial code

---
+++ src/main/java/net/crossover661/endercompass/EnderCompassClient.java
@@ -0,0 +1,9 @@
+package net.crossover661.endercompass;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnderCompassClient implements ClientModInitializer {
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/crossover661/endercompass/EnderCompassDataGenerator.java
@@ -0,0 +1,11 @@
+package net.crossover661.endercompass;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnderCompassDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
